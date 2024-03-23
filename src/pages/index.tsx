92a91d005dd61f3e777e5ea456e92cf26a20cf89
Nikita Naleikin <nnaleikin361@gmail.com>
2024-03-23 22:30:53 +0400

    Fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,14 @@
-import { maskitoPhoneOptionsGenerator } from '@maskito/phone';
-import metadata from 'libphonenumber-js/metadata.max.json';
-
 import { FifthSection } from '@/components/FifthSection';
 import { FirstSection } from '@/components/FirstSection';
 import { FourthSection } from '@/components/FourthSection';
 import { SecondSection } from '@/components/SecondSection';
 import { ThirdSection } from '@/components/ThirdSection';
-import { TextField } from '@/components/_/TextField';
 
 import type { NextPage } from 'next';
-
-const m = maskitoPhoneOptionsGenerator({ metadata, countryIsoCode: 'RU' });
 
 const Page: NextPage = () => (
   <main>
     <FirstSection />
-
-    <div style={{ padding: '2rem', display: 'flex', gap: '1rem' }}>
-      <TextField
-        label="Label"
-        maskOptions={m}
-        supportingText="Supporting text"
-      />
-
-      <TextField aria-invalid label="Label" />
-    </div>
-
     <SecondSection />
     <FifthSection />
     <ThirdSection />
